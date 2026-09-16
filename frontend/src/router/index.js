8cf86e48f7c75f40a61590a1a7798a9d86e6a6fd
Nithaam Julius <nithaamjulius06@gmail.com>
2026-09-16 15:42:05 +0200

    Step 3: add public catalogue route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,47 +2,20 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import AuthSelector from "../components/AuthSelector.vue";
 import HomeView from "../views/HomeView.vue";
+import CatalogueView from "../views/CatalogueView.vue";
 import Login from "../views/Login.vue";
 import Signup from "../views/Signup.vue";
 import ForgotPassword from "../views/ForgotPassword.vue";
 import ResetPassword from "../views/ResetPassword.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: HomeView,
-  },
-
-  {
-    path: "/login-signup",
-    name: "AuthSelector",
-    component: AuthSelector,
-  },
-
-  {
-    path: "/login",
-    name: "Login",
-    component: Login,
-  },
-
-  {
-    path: "/signup",
-    name: "Signup",
-    component: Signup,
-  },
-
-  {
-    path: "/forgot-password",
-    name: "ForgotPassword",
-    component: ForgotPassword,
-  },
-
-  {
-    path: "/reset-password",
-    name: "ResetPassword",
-    component: ResetPassword,
-  },
+  { path: "/", name: "Home", component: HomeView },
+  { path: "/catalogue", name: "Catalogue", component: CatalogueView },
+  { path: "/login-signup", name: "AuthSelector", component: AuthSelector },
+  { path: "/login", name: "Login", component: Login },
+  { path: "/signup", name: "Signup", component: Signup },
+  { path: "/forgot-password", name: "ForgotPassword", component: ForgotPassword },
+  { path: "/reset-password", name: "ResetPassword", component: ResetPassword },
 ];
 
 const router = createRouter({
