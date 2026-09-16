f559fd5eb882272c1089743d0dccf420720408c4
Nithaam Julius <nithaamjulius06@gmail.com>
2026-09-16 19:45:19 +0200

    Register shared group basket route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import AuthSelector from "../components/AuthSelector.vue";
 import HomeView from "../views/HomeView.vue";
 import CatalogueView from "../views/CatalogueView.vue";
+import CartView from "../views/CartView.vue";
 import Login from "../views/Login.vue";
 import Signup from "../views/Signup.vue";
 import ForgotPassword from "../views/ForgotPassword.vue";
@@ -11,6 +12,7 @@ import ResetPassword from "../views/ResetPassword.vue";
 const routes = [
   { path: "/", name: "Home", component: HomeView },
   { path: "/catalogue", name: "Catalogue", component: CatalogueView },
+  { path: "/cart", name: "Cart", component: CartView },
   { path: "/login-signup", name: "AuthSelector", component: AuthSelector },
   { path: "/login", name: "Login", component: Login },
   { path: "/signup", name: "Signup", component: Signup },
