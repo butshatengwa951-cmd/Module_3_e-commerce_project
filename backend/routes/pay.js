571fc203e9066bda181b071b5d1f6b2599170097
butshatengwa951-cmd <butshatengwa951@gmail.com>
2026-09-16 10:30:07 +0200

    Add Payfast checkout routes

--- backend/routes/pay.js
+++ backend/routes/pay.js
@@ -1,1 +1,7 @@
-const e=require('express');const r=e.Router();const c=require('../controllers/payController');r.post('/',c.pay);module.exports=r;
+const e = require("express");
+const r = e.Router();
+const c = require("../controllers/payController");
+r.post("/", c.pay);
+r.post("/payfast", c.createPayfastCheckout);
+r.post("/payfast/notify", e.urlencoded({ extended: false }), c.payfastNotify);
+module.exports = r;
