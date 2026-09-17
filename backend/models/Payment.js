689394ec215b964ab0f959d6d4cc551599ee03af
butshatengwa951-cmd <butshatengwa951@gmail.com>
2026-09-17 08:45:45 +0200

    Remove obsolete card-funded order payment model

--- backend/models/Payment.js
+++
@@ -1,4 +0,0 @@
-// Order checkout is wallet-funded in the Stokvel revamp.
-// Member payment cards are used only by the Stokvel contribution flow in
-// models/StokvelFeatures.js.
-export const walletCheckoutOnly = true;
