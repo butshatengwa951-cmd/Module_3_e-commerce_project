5b2144a922cbbd0afe3ccb05bcd789519a80e121
butshatengwa951-cmd <butshatengwa951@gmail.com>
2026-09-17 07:31:24 +0200

    enable order history delivery navigation

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { attachOrderHistoryNavigation } from "./services/orderHistoryNavigation.js";
 
 import "./assets/styles/theme.css";
 import "./assets/styles/catalogue-overrides.css";
 import "./assets/styles/light-mode-contrast.css";
+
+attachOrderHistoryNavigation();
 
 createApp(App)
   .use(router)
