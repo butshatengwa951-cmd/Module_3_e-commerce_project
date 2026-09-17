2335232d953b69ab766e11650ae9ed6fe928ba55
butshatengwa951-cmd <butshatengwa951@gmail.com>
2026-09-17 06:37:28 +0200

    Add protected order history route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,7 @@ import CatalogueView from "../views/CatalogueView.vue";
 import CartView from "../views/CartView.vue";
 import PaymentView from "../views/PaymentView.vue";
 import ProfileView from "../views/ProfileView.vue";
+import OrderHistoryView from "../views/OrderHistoryView.vue";
 import AdminProfileView from "../views/AdminProfileView.vue";
 import AdminDashboard from "../views/AdminDashboard.vue";
 import AdminSuggestionsView from "../views/AdminSuggestionsView.vue";
@@ -20,6 +21,7 @@ const routes = [
   { path: "/catalogue", name: "Catalogue", component: CatalogueView },
   { path: "/cart", name: "Cart", component: CartView, meta: { requiresAuth: true } },
   { path: "/payment", name: "Payment", component: PaymentView, meta: { requiresAuth: true } },
+  { path: "/order-history", name: "OrderHistory", component: OrderHistoryView, meta: { requiresAuth: true } },
   { path: "/profile", name: "Profile", component: ProfileView, meta: { requiresAuth: true } },
   { path: "/admin/profile", name: "AdminProfile", component: AdminProfileView, meta: { requiresAuth: true, requiresAdmin: true } },
   { path: "/suggestions", name: "Suggestions", component: SuggestionsView, meta: { requiresAuth: true } },
