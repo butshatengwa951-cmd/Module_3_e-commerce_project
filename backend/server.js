7546267947136d5e210d57ccd3c0dc5462f00ded
butshatengwa951-cmd <butshatengwa951@gmail.com>
2026-09-17 12:33:38 +0200

    Register Stokvel proposal and PayFast routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import stokvelRoutes from "./routes/stokvelRoutes.js";
 import stokvelFeaturesRoutes from "./routes/stokvelFeaturesRoutes.js";
+import stokvelProposalRoutes from "./routes/stokvelProposalRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
@@ -13,16 +14,20 @@ import reorderRoutes from "./routes/reorderRoutes.js";
 import paymentRoutes from "./routes/paymentRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import suggestionRoutes from "./routes/suggestionRoutes.js";
+import { payfastNotify } from "./controllers/payfastController.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.get("/", (req, res) => res.json({ success: true, message: "StockWell API is running." }));
+app.post("/api/payment/payfast/notify", payfastNotify);
 app.use("/api/auth", authRoutes);
 app.use("/api/stokvels", stokvelRoutes);
 app.use("/api/stokvel-features", stokvelFeaturesRoutes);
+app.use("/api/stokvel-proposals", stokvelProposalRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
