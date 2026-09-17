3dfc1ccfa01ecebc999815afbe1c837ee84bb0b7
butshatengwa951-cmd <butshatengwa951@gmail.com>
2026-09-17 14:29:59 +0200

    Load shared StockWell styling tokens globally

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import { attachOrderHistoryNavigation } from "./services/orderHistoryNavigation.js";
 
 import "./assets/styles/theme.css";
+import "./assets/styles/consistency.css";
 import "./assets/styles/catalogue-overrides.css";
 import "./assets/styles/light-mode-contrast.css";
 
