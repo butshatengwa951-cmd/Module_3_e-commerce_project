8f47d3c3532405c538355afa05bda9ed9b5804c6
butshatengwa951-cmd <butshatengwa951@gmail.com>
2026-09-17 03:35:05 +0200

    Apply catalogue toolbar filter removal

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/styles/theme.css";
+import "./assets/styles/catalogue-overrides.css";
 
 createApp(App)
   .use(router)
